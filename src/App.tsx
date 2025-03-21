90849fd5efc93d8d72f3a5c4280af3976fa1c28a
Christopher <cnap@udel.edu>
2025-03-21 19:47:09 -0400

    fixed type issues

--- src/App.tsx
+++ src/App.tsx
@@ -4,11 +4,11 @@ import './App.css';
 import { Button, Form } from 'react-bootstrap';
 
 //local storage and API Key: key should be entered in by the user and will be stored in local storage (NOT session storage)
-let keyData = "";
+let keyData: string = "";
 const saveKeyData = "MYKEY";
 const prevKey = localStorage.getItem(saveKeyData); //so it'll look like: MYKEY: <api_key_value here> in the local storage when you inspect
 if (prevKey !== null) {
-  keyData = JSON.parse(prevKey);
+  keyData = JSON.parse(prevKey) as string;
 }
 
 function App() {
