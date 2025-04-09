feae5a26e20856650bd6df7d523082805a65f03a
Murphex5 <jmurph@udel.edu>
2025-04-09 02:10:52 -0400

    Progress bar added to basic and detailed question pages

--- src/components/AdvancedQuestions.tsx
+++ src/components/AdvancedQuestions.tsx
@@ -1,10 +1,20 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import ProgressBar from './ProgressBar';
+import { useState } from 'react';
 
 function AdvancedQuestion() {
+  const [progress, setProgress] = useState<number>(0);
+    
+    const incrementProgress = () => {
+      setProgress((prev: number) => Math.min(prev + 10, 100));
+    
+    };
   return (
     <div>
       <h1>This is the Advanaced Question Page</h1>
+      <ProgressBar progress={progress} />
+      <button onClick={incrementProgress}>Add Progress</button>
       <Link to="/">Back to Home</Link>
     </div>
   );
--- src/components/BasicQuestion.tsx
+++ src/components/BasicQuestion.tsx
@@ -1,13 +1,25 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import ProgressBar from './ProgressBar';
+import { useState } from 'react';
 
 function BasicQuestion() {
+  const [progress, setProgress] = useState<number>(0);
+  
+  const incrementProgress = () => {
+    setProgress((prev: number) => Math.min(prev + 10, 100));
+  
+  };
+  
   return (
     <div>
       <h1>This is the Basic Question Page</h1>
+      <ProgressBar progress={progress} />
+      <button onClick={incrementProgress}>Add Progress</button>
       <Link to="/">Back to Home</Link>
     </div>
   );
 }
 
 export default BasicQuestion;
+
